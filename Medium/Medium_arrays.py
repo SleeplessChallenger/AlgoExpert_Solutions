b90b139b37208f8eddfd66af06bc72dad729438f
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-03-08 10:09:39 +0500

    Update Medium_arrays.py

--- Medium/Medium_arrays.py
+++ Medium/Medium_arrays.py
@@ -83,3 +83,85 @@ def smallestDif(arr1, arr2):
 			smallest = current
 			cont = [first, second]
 	return cont
+
+# 3 Move element to End
+# mine
+def moveElementToEnd(arr, toMove):
+	left = 0
+	right = len(arr) - 1
+	while left <= right:
+		if arr[left] == toMove and arr[right] == toMove:
+			right -= 1
+		elif arr[left] == toMove and arr[right] != toMove:
+			arr[left], arr[right] = arr[right], arr[left]
+			left += 1
+			right -= 1
+		elif arr[left] != toMove and arr[right] != toMove:
+			left += 1
+		else:
+			left += 1
+			right -= 1
+	return arr
+
+# not mine
+def moveElementToEnd(arr, toMove):
+	i = 0
+	j = len(arr) - 1
+	while i < j:
+		while i < j and arr[j] == toMove:
+			j -= 1
+		if arr[i] == toMove:
+			arr[i], arr[j] = arr[j], arr[i]
+		i += 1
+	return arr
+
+# 4 Monotonic array
+# mine
+def isMonotonic(arr):
+	if len(arr) == 0 or len(arr) == 1:
+		return True
+	for x in range(1, len(arr)):
+		if arr[x] > arr[x-1]:
+			for y in range(2, len(arr)):
+				if arr[y] >= arr[y-1]:
+					continue
+				else:
+					return False
+			break
+		elif arr[x] < arr[x-1]:
+			for y in range(2, len(arr)):
+				if arr[y] <= arr[y-1]:
+					continue
+				else:
+					return False
+			break
+	return True
+ # not mine a
+def isMonotonic(arr):
+	nonIncr = True
+	nonDecr = True
+	for x in range(1, len(arr)):
+		if arr[x] > arr[x-1]:
+			nonDecr = False
+		if arr[x] < arr[x-1]:
+			nonIncr = False
+	return nonIncr or nonDecr
+
+# not mine b
+def isMonotonic(arr):
+	if len(arr) <= 2:
+		return True
+	direction = arr[1] - arr[0]
+	for x in range(2, len(arr)):
+		if direction == 0:
+			direction = arr[x] - arr[x-1]
+			continue
+		if func(direction, arr[x-1], arr[x]):
+			return False
+	return True
+
+def func(direction, prev, curr):
+	difference = curr - prev
+	if direction > 0:
+		return difference < 0
+	return difference > 0
