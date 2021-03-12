77c52263fb4074f22345c060bba0f0b2fe037dd3
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-03-12 16:59:07 +0500

    Update Easy_arrays.py

--- Easy/Easy_arrays.py
+++ Easy/Easy_arrays.py
@@ -73,3 +73,30 @@ def helper(score, team, arr):
 	if team not in arr:
 		arr[team] = 0
 	arr[team] += score
+
+# 5 Sorted Squared Array
+# mine
+def sortedSquaredArray(arr):
+	for x in range(len(arr)):
+		arr[x] = arr[x] * arr[x]
+	arr.sort() # for simplicity
+	return arr
+
+# not mine
+def sortedSquaredArray(arr):
+	new_arr = [x for x in range(len(arr))]
+	small = 0
+	large = len(arr) - 1
+
+	for x in reversed(range(len(arr))):
+		small_val = arr[small]
+		large_val = arr[large]
+
+		if abs(small_val) > abs(large_val):
+			new_arr[x] = small_val * small_val
+			small += 1
+		else:
+			new_arr[x] = large_val * large_val
+			large -= 1
+
+	return new_arr
