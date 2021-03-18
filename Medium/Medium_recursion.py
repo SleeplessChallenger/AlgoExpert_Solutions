855f5bfdecba8e70f2fff18fde17e86fed8d28e1
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-03-18 11:05:24 +0500

    Update Medium_recursion.py

--- Medium/Medium_recursion.py
+++ Medium/Medium_recursion.py
@@ -62,3 +62,66 @@ def helper(idx, curr, arr, phone):
 		for x in letters:
 			curr[idx] = x
 			helper(idx+1, curr, arr, phone)
+
+# 4 Staircase traversal
+# recursive with memoization
+def staircaseTraversal(height, maxSteps):
+	return func(height, maxSteps, {0: 1, 1:1})
+
+def func(height, maxSteps, memo):
+	if height in memo:
+		return memo[height]
+	# maxSteps points how many steps 
+	# below we can go and sum their ways 
+	# to receive number of ways for current
+	# step
+	curr = 0
+	for x in range(1, min(height, maxSteps) + 1):
+		curr += func(height - x, maxSteps, memo)
+
+	memo[height] = curr
+	return curr
+
+# iterative with DP
+def staircaseTraversal(height, maxSteps):
+	arr = [0 for x in range(height + 1)]
+	arr[0], arr[1] = 1, 1
+	# + 1 in comprehension is because we
+	# have base case with '0'
+
+	for x in range(2, height + 1):
+		step = 1
+		# indicies in array represent ways (amount of ways)
+		# via which we can reach this step
+		while step <= x and step <= maxSteps:
+		# <= maxSteps: because maxSteps show
+		# how many layers below we can sum to
+		# attain the current level
+			arr[x] = arr[x] + arr[x - step]
+			step += 1
+
+	return arr[height]
+
+# using Sliding window
+def staircaseTraversal(height, maxSteps):
+	arr = [1]
+	curr = 0
+	# ways to get to '0' is '1'
+
+	for x in range(1, height + 1):
+		start = x - maxSteps - 1
+		# 'start' of the previous window
+		# so as to subtract value that is
+		# no longer in our window
+		# '-1' here just enables to get the
+		# value that we are to subtract
+		end = x - 1
+		# to take the previos value
+		# that we need to add to 'curr'
+		if start >= 0:
+			curr -= arr[start]
+
+		curr += arr[end]
+		arr.append(curr)
+
+	return arr[height] # curr also is OK
