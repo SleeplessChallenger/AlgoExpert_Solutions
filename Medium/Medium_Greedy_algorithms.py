47ed1a8972438f8e3fa87ce6b8fe8593c3361581
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-03-23 08:46:44 +0500

    Update Medium_Greedy_algorithms.py

--- Medium/Medium_Greedy_algorithms.py
+++ Medium/Medium_Greedy_algorithms.py
@@ -27,3 +27,46 @@ def helper(arr):
 		else:
 			hashtable[y] += [x]
 	return hashtable
+
+# 2 Valid Starting City
+# T: O(n^2) S: O(1)
+def validStartingCity(distances, fuel, mpg):
+	num_cities = len(distances)
+
+	for x in range(num_cities):
+		miles = 0
+
+		for y in range(x, x + num_cities):
+			if miles < 0:
+				break # == continue
+
+			y = y % num_cities
+			# if we have y > len(distances) - 1
+			# then we need to return to '0' idx
+			fuelCurr = fuel[y]
+			distToNext = distances[y]
+			miles += fuelCurr * mpg - distToNext
+
+		if miles >= 0:
+			return x
+	return -1
+
+# T: O(n) S: O(1)
+def validStartingCity(distances, fuel, mpg):
+	num_cities = len(distances)
+	miles = 0
+
+	milesAtFirst = 0
+	idx = 0
+
+	for x in range(1, num_cities):
+
+		fuelPrev = fuel[x - 1]
+		distPrev = distances[x - 1]
+		miles += mpg * fuelPrev - distPrev
+
+		if miles < milesAtFirst:
+			milesAtFirst = miles
+			idx = x
+
+	return x
