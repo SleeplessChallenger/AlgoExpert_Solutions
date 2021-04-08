37c8b51d826e5d032f78f6253be9061f78887df1
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-08 09:48:00 +0500

    Update Easy_Greedy_algorithms.py

--- Easy/Easy_Greedy_algorithms.py
+++ Easy/Easy_Greedy_algorithms.py
@@ -68,3 +68,40 @@ def classPhotos(reds, blues):
 			if reds[x] <= blues[x]:
 				return False
 	return True
+
+# 3 Tandem bicycle
+# mine
+def tandemBicycle(red, blue, fastest):
+	total = 0
+
+	if fastest:
+		red.sort()
+		blue.sort(reverse=True)
+	else:
+		red.sort()
+		blue.sort()
+
+	for idx in range(len(red)):
+		total += max(red[idx], blue[idx])
+
+	return total
+
+# not mine
+def tandemBicycle(red, blue, fastest):
+	red.sort()
+	blue.sort()
+	if fastest is not True:
+		helper(red)
+
+	total = 0
+	for idx in range(len(blue)):
+		total += max(red[idx], blue[len(blue) - idx - 1])
+	return total
+
+def helper(arr):
+	i = 0
+	j = len(arr) - 1
+	while i < j:
+		arr[i], arr[j] = arr[j], arr[i]
+		i += 1
+		j -= 1
