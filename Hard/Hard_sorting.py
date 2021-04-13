e58e5b4c5c561a2f5534251944167ee27b171606
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-13 10:06:38 +0500

    Update Hard_sorting.py

--- Hard/Hard_sorting.py
+++ Hard/Hard_sorting.py
@@ -52,3 +52,35 @@ def sort(arr, start, end):
 
 def swap(arr, i, j):
 	arr[i], arr[j] = arr[j], arr[i]
+
+
+# 2 Heap Sort
+def heapSort(array):
+	buildHeap(array)
+	for idx in reversed(range(1, len(array))):
+		swap_func(0, idx, array)
+		siftDown(0, idx - 1, array)
+	return array
+
+def buildHeap(heap):
+	parentIdx = (len(heap) - 2) // 2 # -1 also OK
+	for idx in reversed(range(parentIdx + 1)):
+		siftDown(idx, len(heap) - 1, heap)
+
+def siftDown(idx, length, heap):
+	idxOne = idx * 2 + 1
+	while idxOne <= length:
+		idxTwo = idx * 2 + 2 if idx * 2 + 2 <= length else -1
+		if idxTwo != -1 and heap[idxTwo] > heap[idxOne]:
+			swap = idxTwo
+		else:
+			swap = idxOne
+		if heap[swap] > heap[idx]:
+			swap_func(swap, idx, heap)
+			idx = swap
+			idxOne = idx * 2 + 1
+		else:
+			return 
+
+def swap_func(i, j, heap):
+	heap[i], heap[j] = heap[j], heap[i]
