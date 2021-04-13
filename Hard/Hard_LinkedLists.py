b6d1db9e72f2f885766b7be0109807b9282d67de
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-13 08:30:12 +0500

    Add files via upload

---
+++ Hard/Hard_LinkedLists.py
@@ -0,0 +1,50 @@
+# 1 Find Loop
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# T: O(n S: O(n)
+def findLoop(head):
+	ht = {}
+
+	while True:
+		if head not in ht:
+			ht[head] = True
+			head = head.next
+		else:
+			return head
+
+# T: O(n) S: O(1)
+def findLoop(head):
+	first = head
+	second = head
+
+	while True:
+		first = first.next
+		second = second.next.next
+		if first == second:
+			break
+
+	first = head
+	while first != second:
+		first = first.next
+		second = second.next
+
+	return first
+
+# same as above just minor tweaks
+def findLoop(head):
+	first = head.next
+	second = head.next.next
+
+	while first != second:
+		first = first.next
+		second = second.next.next
+
+	first = head
+	while first != second:
+		first = first.next
+		second = second.next
+
+	return first
