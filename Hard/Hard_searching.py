a00e630fb88b72de361124fa9800f52c73c500fd
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-14 11:38:18 +0500

    Add files via upload

---
+++ Hard/Hard_searching.py
@@ -0,0 +1,31 @@
+# 1 Shifted Binary Search
+def shiftedBinarySearch(array, target):
+	# : O(n) S: O(1)
+	if target in array:
+		return array.index(target)
+	return -1
+
+	# T: O(log n) S: O(1)
+	left = 0
+	right = len(array) - 1
+
+	while left <= right:
+		middle = (left + right) // 2
+
+		if array[middle] == target:
+			return middle
+
+		elif array[middle] >= array[left]:
+			if target >= array[left] and target < array[middle]:
+				right = middle - 1
+			else:
+				left = middle + 1
+
+		elif array[middle] < array[right]:
+		# else
+			if array[middle] < target and target <= array[right]:
+				left = middle + 1
+			else:
+				right = middle - 1
+
+	return -1
