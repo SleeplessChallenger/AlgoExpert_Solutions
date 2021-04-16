99e2cb20ced357a9dd76c172305313144eb477ce
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-16 11:40:22 +0500

    Update Hard_arrays.py

--- Hard/Hard_arrays.py
+++ Hard/Hard_arrays.py
@@ -60,3 +60,57 @@ def largestRange(array):
 			result = [left + 1, right - 1]
 
 	return result
+
+# 2
+def minRewards(scores):
+	rewards = [1 for x in scores]
+	localMins = getLocals(scores)
+	for local in localMins:
+		explore(local, scores, rewards)
+	return sum(rewards)
+
+def getLocals(scores):
+	if len(scores) == 1:
+		return [0]
+	cont = []
+	for idx in range(len(scores)):
+		if idx == 0 and scores[idx] < scores[idx+1]:
+			cont.append(idx)
+		if idx == len(scores) - 1 and scores[idx] < scores[idx-1]:
+			cont.append(idx)
+		if idx == 0 or idx == len(scores) - 1:
+			continue
+		if scores[idx] < scores[idx-1] and scores[idx] > scores[idx+1]:
+			cont.append(idx)
+	return cont
+
+def explore(local, scores, rewards):
+	leftIdx = local - 1
+	rightIdx = local + 1
+
+	while leftIdx >= 0 and scores[leftIdx] < scores[leftIdx+1]:
+		rewards[leftIdx] = max(rewards[leftIdx], rewards[leftIdx+1]+1)
+		leftIdx -= 1
+
+	while rightIdx <= len(scores) - 1 and scores[rightIdx] > scores[rightIdx-1]:
+		rewards[rightIdx] = rewards[rightIdx-1] + 1
+		# because when to 'right' we assign only once
+		# but when to 'left' then we may reassign rewards existing
+		rightIdx += 1
+
+# 3
+def minRewards(scores):
+	rewards = [1 for x in scores]
+
+	for idx in range(1, len(scores)):
+		if scores[idx] > scores[idx-1]:
+			rewards[idx] = rewards[idx-1] + 1
+		# because when to 'right' we assign only once
+		# but when to 'left' then we may reassign rewards existing
+		# => no max() required
+
+	for idx in reversed(range(len(scores) - 1)):
+		if scores[idx] > scores[idx+1]:
+			rewards[idx] = max(rewards[idx], rewards[idx+1]+1)
+
+	return sum(rewards)
