edb4aea3fe2292c5f328da460315fc20815a6c10
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-27 11:16:03 +0500

    Update Hard_strings.py

--- Hard/Hard_strings.py
+++ Hard/Hard_strings.py
@@ -74,3 +74,124 @@ def underScorify(array, string):
 	elif strIdx < len(string):
 		result.append(string[strIdx:])
 	return ''.join(result)
+
+# 3 Pattern Matcher
+# mine
+def patternMatcher(pattern, string):
+	if len(pattern) > len(string):
+		return []
+	new_pattern = checkPattern(pattern)
+	ht, firstY = getPosition(new_pattern)
+	wasSwap = new_pattern[0] != pattern[0]
+
+	if 'y' in ht and ht['y'] > 0:
+		for xIdx in range(1, len(string)):
+			lenY = (len(string) - xIdx * ht['x'])/ht['y']
+			if lenY <= 0 or lenY % 1 != 0:
+				continue
+			lenY = int(lenY)
+			yIdx = firstY * xIdx
+			xString = string[:xIdx]
+			yString = string[yIdx:yIdx + lenY]
+			temp = map(lambda token: xString if token == 'x'
+											 else yString,
+											 new_pattern)
+			if string == ''.join(temp):
+				return [xString, yString] if not wasSwap\
+										  else [yString, xString]
+	else:
+		lenX = len(string)/ht['x']
+		if lenX % 1 == 0:
+			lenX = int(lenX)
+			xString = string[:lenX]
+			temp = map(lambda token: xString, new_pattern)
+			if string == ''.join(temp):
+				return [xString, ''] if not wasSwap\
+									 else ['', xString]
+	return []
+
+def checkPattern(pattern):
+	if pattern[0] == 'x':
+		return list(pattern)
+	new_pattern = []
+	for x in pattern:
+		if x == 'x':
+			new_pattern.append('y')
+		elif x == 'y':
+			new_pattern.append('x')
+	return new_pattern
+
+def getPosition(pattern):
+	ht = {}
+	for x in pattern:
+		if x not in ht:
+			ht[x] = 0
+		ht[x] += 1
+	pos = pattern.index('y') if 'y' in pattern\
+							 else -1
+	return ht, pos
+
+# not mine
+def patternMatcher(pattern, string):
+	if len(pattern) > len(string):
+		return []
+	new_pattern = checkPattern(pattern)
+	ht = {'x': 0, 'y': 0}
+	firstY = getPosition(new_pattern, ht)
+	wasSwap = new_pattern[0] != pattern[0]
+
+	if ht['y'] > 0:
+		for xIdx in range(1, len(string)):
+			lenY = (len(string) - xIdx * ht['x'])/ht['y']
+			if lenY <= 0 or lenY % 1 != 0:
+				continue
+			lenY = int(lenY)
+			yIdx = firstY * xIdx
+			xString = string[:xIdx]
+			yString = string[yIdx:yIdx + lenY]
+			temp = map(lambda token: yString if token == 'y'
+											 else xString,
+											 new_pattern)
+			if string == ''.join(temp):
+				return [xString, yString] if not wasSwap\
+										  else [yString, xString]
+	else:
+		lenX = len(string)/ht['x']
+		if lenX % 1 == 0:
+			lenX = int(lenX)
+			xString = string[:lenX]
+			temp = map(lambda token: xString, new_pattern)
+			if string == ''.join(temp):
+				return [xString, ''] if not wasSwap\
+									 else ['', xString]
+	return []
+
+def checkPattern(pattern):
+	pattern = list(pattern)
+	if pattern[0] == 'x':
+		return pattern
+	return list(map(lambda token: 'x' if token == 'y'
+									  else 'y', pattern))
+
+def getPosition(pattern, ht):
+	pos = None
+	for idx in range(len(pattern)):
+		ht[pattern[idx]] += 1
+		if pattern[idx] == 'y' and not pos:
+			pos = idx
+	return pos
+
+# 1 check whether pattern[0] is x or y
+#	if x: return elif y: change y for x and vice versa
+# 2 count the number of x and y (ht); get position
+#   of first y in the pattern
+# 3 if y > 0: (len(string) - len(x) * num of x)/ num of y
+#   check if resulted number positive and not a decimal
+#
+#	yIdx = firstYPos (in pattern) * len of x
+#	yIdx = 2 * 1 = 2
+# 4 x = string[0:len(x)]
+#	y = string[yIdx:len of Y]
+# 5 replace in pattern if with new X and the same with y
+# 	and check if it == string
+#   BUT: if you've changed y for x in pattern: swap here
