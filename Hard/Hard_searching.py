7404ad9054d8cbcba72b952a300144ad84aa90e0
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-04-29 10:34:23 +0500

    Update Hard_searching.py

--- Hard/Hard_searching.py
+++ Hard/Hard_searching.py
@@ -79,3 +79,79 @@ def helper(array, target, left, right, idxRange, goLeft):
 					return
 				else:
 					left = middle + 1
+
+# 3 Quickselect
+# mine
+def quickselect(array, k):
+	k = k - 1
+	return sort(array, 0, len(array) - 1, k)
+
+def sort(arr, start, end, idx):
+	pivot = start
+	left = start + 1
+	right = end
+
+
+	while left <= right:
+		if arr[pivot] < arr[left] and arr[pivot] > arr[right]:
+			swap(arr, left, right)
+		if arr[pivot] >= arr[left]:
+			left += 1
+		if arr[pivot] <= arr[right]:
+			right -= 1
+	swap(arr, right, pivot)
+	# if our Pivot number
+	# (right) > kth largest => disregard right part
+	if right > idx:
+		# end = right - 1
+		return sort(arr, 0, right - 1, idx)
+	# elif Pivot number < kth => disrgard left part
+	elif right < idx:
+		# start = right + 1
+		return sort(arr, right + 1, end, idx)
+	else:
+		return arr[idx]
+# we need to 'return' on all functions, because without it
+# result of the arr[idx] will get lost in the
+# call stack as those two functions don't return
+# anything by themselves
+
+def swap(arr, i, j):
+	arr[i], arr[j] = arr[j], arr[i]
+
+# not mine
+def quickselect(array, k):
+	k = k - 1
+	return sort(array, 0, len(array) - 1, k)
+
+def sort(arr, start, end, idx):
+	while True:
+		
+		pivot = start
+		left = start + 1
+		right = end
+
+
+		while left <= right:
+			if arr[pivot] < arr[left] and arr[pivot] > arr[right]:
+				swap(arr, left, right)
+			if arr[pivot] >= arr[left]:
+				left += 1
+			if arr[pivot] <= arr[right]:
+				right -= 1
+		swap(arr, right, pivot)
+		# if our Pivot number
+		# (right) > kth largest => disregard right part
+		if right > idx:
+			end = right - 1
+			# sort(arr, 0, right - 1, idx)
+		# elif Pivot number < kth => disrgard left part
+		elif right < idx:
+			start = right + 1
+			# sort(arr, right + 1, end, idx)
+		else:
+			return arr[idx]
+
+
+def swap(arr, i, j):
+	arr[i], arr[j] = arr[j], arr[i]
