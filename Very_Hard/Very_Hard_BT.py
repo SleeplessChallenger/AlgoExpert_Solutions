22f66fa72380e3083f0cd04a668e3447a8c6a749
Daniil Slobodeniuk <76875535+SleeplessChallenger@users.noreply.github.com>
2021-07-02 22:52:08 +0300

    Update Very_Hard_BT.py

--- Very_Hard/Very_Hard_BT.py
+++ Very_Hard/Very_Hard_BT.py
@@ -70,3 +70,139 @@ def flattenTree(root):
 def createConnection(left, right):
 	left.right = right
 	right.left = left
+
+# 3 Right Sibling Tree
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def rightSiblingTree(root):
+    traverse(root, None, None)
+	return root
+
+def traverse(node, p, leftTrue):
+	if node is None:
+		return
+	left, right = node.left, node.right
+	traverse(left, node, True)
+	
+	if p is None:
+		# to deal when we're at root
+		node.right = None
+	elif leftTrue:
+		# as we call `traverse()` on the left
+		# subtree first, then we won't overwrite
+		# `.right` pointer and it'll still point
+		# at `.right` INITIAL value
+		node.right = p.right
+	else:
+		if p.right is None:
+			# Ex: when we're at `9`,
+			# parent is `4`. Let's suppose
+			# it doesn't have `5` as `.right`
+			# hence calling `parent.right.left`
+			# will give error. Use this condition
+			# to bypass it.  
+			# Viable example: `3` has `1` as parent,
+			# `7` has `3` as parent. None of those parents
+			# have `.right.left` + before we reach `7`, `3`
+			# will be as right node as well. `1` initially being
+			# `node` will have `.right` = None, hence `3` will also
+			# have `.right` = None. Hence `7` will resemble
+			node.right = None
+		else:
+	        node.right = p.right.left
+		
+	traverse(right, node, False)
+
+# 4 All Kinds of Node Depths
+# mine
+# T: O(n*log(n)) S: O(n)
+def allKindsOfNodeDepths(root):
+	arr = []
+	def dfs(node, arr):
+		arr.append(nodeDepth(node))
+		if node.left:
+			dfs(node.left, arr)
+		if node.right:
+			dfs(node.right, arr)
+	dfs(root, arr)
+	return sum(arr)
+
+def nodeDepth(node, depth=0):
+	if node is None:
+		return 0
+	return depth + nodeDepth(node.left, depth+1) + \
+			nodeDepth(node.right, depth+1)
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# 2  solutions which are not mine, but same complexity
+# 2-1
+def allKindsOfNodeDepths(root):
+	count = 0
+	stack = [root]
+	while len(stack) != 0:
+		node = stack.pop()
+		if node is None:
+			continue
+		count += nodeDepth(node)
+		stack.append(node.left)
+		stack.append(node.right)
+
+# 2-2
+def allKindsOfNodeDepths(root):
+	if root is None:
+		return 0
+	return allKindsOfNodeDepths(root.left) +\
+		   allKindsOfNodeDepths(root.right) +\
+		   nodeDepth(root)
+
+# T: O(n) S: O(n)
+def allKindsOfNodeDepths(root):
+    ht = {}
+	countNodes(root, ht)
+	htD = {}
+	countDepths(root, ht, htD)
+	return getResult(htD, root)
+
+def getResult(ht, node):
+	if node is None:
+		return 0
+	return getResult(ht, node.left) +\
+		   getResult(ht,  node.right) +\
+		   ht[node]
+	
+def countNodes(node, ht):
+	# build every node `count` starting
+	# from bottom (we go till bottom and
+	# then start from there). Also, before
+	# looking for `.left`. or. `.right` we need
+	# to place `1` as current node is 1 count.
+	ht[node] = 1
+	if node.left:
+		countNodes(node.left, ht)
+		ht[node] += ht[node.left]
+	if node.right:
+		countNodes(node.right, ht)
+		ht[node] += ht[node.right]
+
+def countDepths(root, ht, htD):
+	# from `2` as root: 2 + 2 + 1 + 1 + 0
+	# from `1` as root: 3 + 3 + 2 + 2 + 1
+	# or we can use `2 + 2 + 1 + 1 + 0` but instead
+	# of 0 add number of nodes in the subtree.
+	htD[root] = 0
+	if root.left:
+		countDepths(root.left, ht, htD)
+		htD[root] += ht[root.left] + htD[root.left]
+	if root.right:
+		countDepths(root.right, ht, htD)
+		htD[root] += ht[root.right] + htD[root.right]
